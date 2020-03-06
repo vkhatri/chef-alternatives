c446fd7a37017b87048e7344a3105ea7d0f91d09
Tim Smith <tsmith84@gmail.com>
2020-03-06 13:21:09 -0800

    Switch to why-run capable assertions
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/default.rb
+++ resources/default.rb
@@ -3,9 +3,27 @@ property :link, String, default: lazy { |n| "/usr/bin/#{n.link_name}" }
 property :path, String
 property :priority, [String, Integer], coerce: proc { |n| n.to_i }
 
+def define_resource_requirements
+  requirements.assert(:install, :set, :remove) do |a|
+    a.assertion do
+      !new_resource.path.nil?
+    end
+
+    a.failure_message("Could not set alternatives for #{new_resource.link_name}, must provide :path property")
+  end
+
+  requirements.assert(:install, :set, :remove) do |a|
+    a.assertion do
+      ::File.exist?(new_resource.path)
+    end
+
+    a.whyrun("Assuming file #{new_resource.path} already exists or was created already")
+    a.failure_message("Could not set alternatives for #{new_resource.link_name}, missing #{new_resource.path}")
+  end
+end
+
 action :install do
   raise 'missing :priority' unless new_resource.priority
-  validate_path
 
   if path_priority != new_resource.priority
     converge_by("adding alternative #{new_resource.link} #{new_resource.link_name} #{new_resource.path} #{new_resource.priority}") do
@@ -18,8 +36,6 @@ action :install do
 end
 
 action :set do
-  validate_path
-
   if current_path != new_resource.path
     converge_by("setting alternative #{new_resource.link_name} #{new_resource.path}") do
       output = shell_out("#{alternatives_cmd} --set #{new_resource.link_name} #{new_resource.path}")
@@ -31,8 +47,6 @@ action :set do
 end
 
 action :remove do
-  validate_path
-
   if path_exists?
     converge_by("removing alternative #{new_resource.link_name} #{new_resource.path}") do
       shell_out("#{alternatives_cmd} --remove #{new_resource.link_name} #{new_resource.path}")
@@ -61,15 +75,6 @@ action_class do
     end
   end
 
-  def validate_path
-    unless new_resource.path
-      raise "could not set alternatives for #{new_resource.link_name}, must provide :path"
-    end
-    unless ::File.exist?(new_resource.path)
-      raise "could not set alternatives for #{new_resource.link_name}, missing #{new_resource.path}"
-    end
-  end
-
   def path_priority
     # https://rubular.com/r/IcUlEU0mSNaMm3
     escaped_path = Regexp.new(Regexp.escape("#{new_resource.path} - priority ") + '(.*)')
@@ -87,6 +92,6 @@ action_class do
   def path_exists?
     # https://rubular.com/r/ogvDdq8h2IKRff
     escaped_path = Regexp.new(Regexp.escape("#{new_resource.path} - priority"))
-    shell_out("#{alternatives_cmd} --display #{new_resource.link_name}").stdout.match?(escaped_path)
+    shell_out("#{alternatives_cmd} --display #{new_resource.link_name}").stdout.match(escaped_path)
   end
 end
